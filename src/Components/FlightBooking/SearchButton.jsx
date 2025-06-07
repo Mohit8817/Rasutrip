98fff9ba4d3d8e64fffa9568825d85c4adea17a6
HarmonicCode <sharmasimran91262@gmail.com>
2025-06-07 16:58:46 +0530

    uiii

--- src/Components/FlightBooking/SearchButton.jsx
+++ src/Components/FlightBooking/SearchButton.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../Style/FlightBooking.css'; // Add your custom styles
 
 const SearchButton = () => {
+    const navigate = useNavigate();
+
+    const handleSearch = () => {
+        navigate('/hotels'); // Make sure '/hotels' route is defined in your router
+    };
+
     return (
         <div>
-
-            <button className="Mybutton">
+            <button className="Mybutton" onClick={handleSearch}>
                 Search
             </button>
-
         </div>
-    )
-}
+    );
+};
 
-export default SearchButton
+export default SearchButton;
